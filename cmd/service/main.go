42086ff4ff8313622be1166e22a9671e8858fbc2
hutamy <hutamytriesthi@gmail.com>
2023-11-13 22:47:20 +0700

    initialize redis client

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"ohlc/config"
+	"ohlc/redis"
+	"time"
 )
 
 func main() {
-	config.SetConfig()
+	cfg := config.SetConfig()
+	ctx := context.Background()
+	_, err := redis.NewRedisClient(ctx, cfg.RedisHost, cfg.RedisPassword, cfg.RedisPort)
+	if err != nil {
+		panic(err)
+	}
+
 	// Create a new web server
 	http.HandleFunc("/ohlc", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode("ok")
+		if err = json.NewEncoder(w).Encode(map[string]interface{}{"ok": true}); err != nil {
+			panic(err)
+		}
 	})
 
-	// Start the web server and listen for incoming requests
-	http.ListenAndServe(":8080", nil)
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ redis/redis.go
@@ -0,0 +1,31 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient(ctx context.Context, host, password string, port int) (*RedisClient, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", host, port),
+		Password: password,
+		DB:       0,
+	})
+
+	log.Println("Redis Connection: ", client.Ping(ctx))
+	if err := client.Ping(ctx).Err(); err != nil {
+		log.Println("Redis Connection Error Log: ", err)
+		return nil, err
+	}
+
+	return &RedisClient{
+		client: client,
+	}, nil
+}
